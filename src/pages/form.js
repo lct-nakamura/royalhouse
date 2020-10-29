236350f339c0e7272f50ba8ed4a734b394420f54
Chiho U <chiho8052@gmail.com>
2020-10-29 14:48:32 +0100

    [fix]フォーム系修正、再ビルド

--- src/pages/form.js
+++ src/pages/form.js
@@ -44,7 +44,7 @@ const SecondPage = () => (
             </section>
             <section className="article__section article__section02">
               <h2>プレミアム・カタログ・資料請求フォーム</h2>
-              <form id="mailformpro" action="mail.php" method="POST">
+              <form id="mailformpro" action="../mail.php" method="POST">
                 <div className="form-group row">
                   <div className="col-12 col-sm-4 required"><label for="MfpField_01">お名前</label></div>
                   <div className="input-group col-12 col-sm-8 ">
@@ -67,7 +67,7 @@ const SecondPage = () => (
                   <div className="form-group row">
                     <div className="col-12 col-sm-4 required"><label for="MfpField_04">郵便番号</label></div>
                     <div className="input-group col-12 col-sm-8 ">
-                      <input type="text" className="form-control" id="MfpField_04" placeholder="000-0000" name="郵便番号" data-address="都道府県,市区町村,市区町村" />
+                      <input type="text" className="form-control" id="MfpField_04" placeholder="000-0000" name="郵便番号" data-address="都道府県,市区町村,市区町村" required="required" />
                     </div>
                   </div>
                 </div>
@@ -76,68 +76,68 @@ const SecondPage = () => (
                   <div className="input-group col-12 col-sm-8 ">
                     <div className="w-100">
                       <label for="MfpField_05">都道府県</label>
-                      <select className="custom-select" id="MfpField_05" name="都道府県" required="required">
+                      <select className="custom-select" id="MfpField_05" name="都道府県[]" required="required">
                         <option value="" defaultValue="selected">【選択して下さい】</option>
                         <optgroup label="北海道・東北地方">
-                          <option value="北海道" name="都道府県[]">北海道</option>
-                          <option value="青森県" name="都道府県[]">青森県</option>
-                          <option value="岩手県" name="都道府県[]">岩手県</option>
-                          <option value="秋田県" name="都道府県[]">秋田県</option>
-                          <option value="宮城県" name="都道府県[]">宮城県</option>
-                          <option value="山形県" name="都道府県[]">山形県</option>
-                          <option value="福島県" name="都道府県[]">福島県</option>
+                          <option value="北海道">北海道</option>
+                          <option value="青森県">青森県</option>
+                          <option value="岩手県">岩手県</option>
+                          <option value="秋田県">秋田県</option>
+                          <option value="宮城県">宮城県</option>
+                          <option value="山形県">山形県</option>
+                          <option value="福島県">福島県</option>
                         </optgroup>
                         <optgroup label="関東地方">
-                          <option value="栃木県" name="都道府県[]">栃木県</option>
-                          <option value="群馬県" name="都道府県[]">群馬県</option>
-                          <option value="茨城県" name="都道府県[]">茨城県</option>
-                          <option value="埼玉県" name="都道府県[]">埼玉県</option>
-                          <option value="東京都" name="都道府県[]">東京都</option>
-                          <option value="千葉県" name="都道府県[]">千葉県</option>
-                          <option value="神奈川県" name="都道府県[]">神奈川県</option>
+                          <option value="栃木県">栃木県</option>
+                          <option value="群馬県">群馬県</option>
+                          <option value="茨城県">茨城県</option>
+                          <option value="埼玉県">埼玉県</option>
+                          <option value="東京都">東京都</option>
+                          <option value="千葉県">千葉県</option>
+                          <option value="神奈川県">神奈川県</option>
                         </optgroup>
                         <optgroup label="中部地方">
-                          <option value="山梨県" name="都道府県[]">山梨県</option>
-                          <option value="長野県" name="都道府県[]">長野県</option>
-                          <option value="新潟県" name="都道府県[]">新潟県</option>
-                          <option value="富山県" name="都道府県[]">富山県</option>
-                          <option value="石川県" name="都道府県[]">石川県</option>
-                          <option value="福井県" name="都道府県[]">福井県</option>
-                          <option value="静岡県" name="都道府県[]">静岡県</option>
-                          <option value="岐阜県" name="都道府県[]">岐阜県</option>
-                          <option value="愛知県" name="都道府県[]">愛知県</option>
+                          <option value="山梨県">山梨県</option>
+                          <option value="長野県">長野県</option>
+                          <option value="新潟県">新潟県</option>
+                          <option value="富山県">富山県</option>
+                          <option value="石川県">石川県</option>
+                          <option value="福井県">福井県</option>
+                          <option value="静岡県">静岡県</option>
+                          <option value="岐阜県">岐阜県</option>
+                          <option value="愛知県">愛知県</option>
                         </optgroup>
                         <optgroup label="近畿地方">
-                          <option value="三重県" name="都道府県[]">三重県</option>
-                          <option value="滋賀県" name="都道府県[]">滋賀県</option>
-                          <option value="京都府" name="都道府県[]">京都府</option>
-                          <option value="大阪府" name="都道府県[]">大阪府</option>
-                          <option value="兵庫県" name="都道府県[]">兵庫県</option>
-                          <option value="奈良県" name="都道府県[]">奈良県</option>
-                          <option value="和歌山県" name="都道府県[]">和歌山県</option>
+                          <option value="三重県">三重県</option>
+                          <option value="滋賀県">滋賀県</option>
+                          <option value="京都府">京都府</option>
+                          <option value="大阪府">大阪府</option>
+                          <option value="兵庫県">兵庫県</option>
+                          <option value="奈良県">奈良県</option>
+                          <option value="和歌山県">和歌山県</option>
                         </optgroup>
                         <optgroup label="四国地方">
-                          <option value="徳島県" name="都道府県[]">徳島県</option>
-                          <option value="香川県" name="都道府県[]">香川県</option>
-                          <option value="愛媛県" name="都道府県[]">愛媛県</option>
-                          <option value="高知県" name="都道府県[]">高知県</option>
+                          <option value="徳島県">徳島県</option>
+                          <option value="香川県">香川県</option>
+                          <option value="愛媛県">愛媛県</option>
+                          <option value="高知県">高知県</option>
                         </optgroup>
                         <optgroup label="中国地方">
-                          <option value="鳥取県" name="都道府県[]">鳥取県</option>
-                          <option value="島根県" name="都道府県[]">島根県</option>
-                          <option value="岡山県" name="都道府県[]">岡山県</option>
-                          <option value="広島県" name="都道府県[]">広島県</option>
-                          <option value="山口県" name="都道府県[]">山口県</option>
+                          <option value="鳥取県">鳥取県</option>
+                          <option value="島根県">島根県</option>
+                          <option value="岡山県">岡山県</option>
+                          <option value="広島県">広島県</option>
+                          <option value="山口県">山口県</option>
                         </optgroup>
                         <optgroup label="九州・沖縄地方">
-                          <option value="福岡県" name="都道府県[]">福岡県</option>
-                          <option value="佐賀県" name="都道府県[]">佐賀県</option>
-                          <option value="長崎県" name="都道府県[]">長崎県</option>
-                          <option value="大分県" name="都道府県[]">大分県</option>
-                          <option value="熊本県" name="都道府県[]">熊本県</option>
-                          <option value="宮崎県" name="都道府県[]">宮崎県</option>
-                          <option value="鹿児島県" name="都道府県[]">鹿児島県</option>
-                          <option value="沖縄県" name="都道府県[]">沖縄県</option>
+                          <option value="福岡県">福岡県</option>
+                          <option value="佐賀県">佐賀県</option>
+                          <option value="長崎県">長崎県</option>
+                          <option value="大分県">大分県</option>
+                          <option value="熊本県">熊本県</option>
+                          <option value="宮崎県">宮崎県</option>
+                          <option value="鹿児島県">鹿児島県</option>
+                          <option value="沖縄県">沖縄県</option>
                         </optgroup>
                       </select>
                     </div>
@@ -154,13 +154,13 @@ const SecondPage = () => (
                 <div className="form-group row">
                   <div className="col-12 col-sm-4 required"><label for="MfpField_08">電話番号</label></div>
                   <div className="input-group col-12 col-sm-8 ">
-                    <input type="tel" id="MfpField_08" data-type="tel" name="電話番号" size="16" data-min="9" className="form-control" className="mfp_element_17" placeholder="000-0000-0000" style={{width: "96px"}} />
+                    <input type="tel" id="MfpField_08" data-type="tel" name="電話番号" size="16" data-min="9" className="form-control" className="mfp_element_17" placeholder="000-0000-0000" required="required" style={{width: "96px"}} />
                   </div>
                 </div>
                 <div className="form-group row">
                   <div className="col-12 col-sm-4 required"><label for="MfpField_09">ご職業</label></div>
                   <div className="input-group col-12 col-sm-8 ">
-                    <select className="custom-select" id="MfpField_09" name="ご職業" data-search-for="MfpField_15">
+                    <select className="custom-select" id="MfpField_09" name="ご職業" data-search-for="MfpField_15" required="required">
                       <option value="" defaultValue="selected">【選択して下さい】</option>
                       <option value="会社員">会社員 </option>
                       <option value="会社役員">会社役員 </option>
@@ -175,11 +175,11 @@ const SecondPage = () => (
                     <label for="MfpField_10">今後ロイヤルハウスからの<br />最新情報をお送りしてもよろしいですか？</label>
                   </div>
                   <div className="form-check">
-                    <input className="form-check-input" type="radio" name="radio1" id="MfpField_10a" checked />
+                    <input className="form-check-input" type="radio" name="最新情報送信[]" value="はい" id="MfpField_10a" checked />
                     <label className="form-check-label" for="MfpField_10a">はい</label>
                   </div>
                   <div className="form-check">
-                    <input className="form-check-input" type="radio" name="radio1" id="MfpField_10b" />
+                    <input className="form-check-input" type="radio" name="最新情報送信[]" value="いいえ" id="MfpField_10b" />
                     <label className="form-check-label" for="MfpField_10b">いいえ</label>
                   </div>
                 </div>

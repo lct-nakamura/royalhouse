7ff3c6d4b361602af45b040a68e87172bf63f962
Gentaro Nakamura <gentaro@lct.jp>
2020-12-10 10:41:47 +0900

    public削除

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,10 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"","short_name":"","start_url":"/","background_color":"","theme_color":"","display":"","icon":"src/images/favicon.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"8dfb0a5ca973885e957a0de084ad81f0"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-google-tagmanager/gatsby-browser.js'),
+      options: {"plugins":[],"id":"GTM-M2LW29B","includeInDevelopment":false,"defaultDataLayer":{"type":"object","value":{"platform":"gatsby"}}},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]

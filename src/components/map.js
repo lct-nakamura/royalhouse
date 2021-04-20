a6f2e5ca18fcddd32109a163161d4c6456106c37
*TcI6Sr8Es3 <gentaro@lct.jp>
2021-04-20 20:46:13 +0900

    public削除

--- src/components/map.js
+++ src/components/map.js
@@ -77,7 +77,7 @@ const Map = () => (
                 </Link>
             </div>
             <div className="map-balloon pt14">
-                <Link to="/vr-tag" className="map-balloon__link" activeClassName="is-current">
+                <Link to="https://www.royal-house.co.jp/vr-movie/" className="map-balloon__link" activeClassName="is-current">
                     <p>全国のVRモデル事例集</p>
                 </Link>
             </div>

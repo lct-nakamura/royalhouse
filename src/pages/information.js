7189ab6a3312836e9c66a099b99b08a2adce9242
福田 翔太 <shouta@lct.jp>
2023-10-02 14:56:17 +0900

    テキスト修正

--- src/pages/information.js
+++ src/pages/information.js
@@ -32,7 +32,7 @@ const SecondPage = () => (
               <ul className="info-bnr__list">
                 <li><h3>１ながら美容パッケージ</h3></li>
                 <li className="center"><img src={withPrefix("/images/information/livgrand_img01.jpg")} alt="１ながら美容パッケージ 毎日の習慣が健康と美容活動に！自宅が SPA空間に代わるアイテムと、浄活水で家中の水がミネラルウォーターの様になるアイテム。 / ➀微細な泡で体中キレイに 「ミラバス」 / ②シャワーヘッド型美顔器 「ミラブル ZERO」 / ③家中キレイな水を提供 「美fine ウォーター」" /></li>
-                <li><h3>2家事業パッケージ</h3></li>
+                <li><h3>2家事楽パッケージ</h3></li>
                 <li className="center"><img src={withPrefix("/images/information/livgrand_img02.jpg")} alt="2家事業パッケージ 時短・手間楽・キレイつづくアイテム!デザイン性が高く、高性能な設備を厳選。家を楽にストレスなく家事ができるアイテム。 / ➀自動洗浄で快適浴槽 「AX」 / ②家族で使えるキッチン「ES」 「ノクト」 / ③掃除も収納も楽ちん! 洗面化粧台 「EV」 / ④ トイレ空間がすっきり 「ペーシアハーモノ」" /></li>
                 <li><h3>3自然素材パッケージ</h3>{/* <p className="h3 mb-5">家事楽動線をデザインした家族でワイワイにぎやかな空間に</p> */}</li>
                 <li className="center"><img src={withPrefix("/images/information/livgrand_img03.jpg")} alt="3自然素材パッケージ 本物の素材感と快適さにこだわる無垢の木や塗り壁など素材にこだわる上質な空間へ空間をすてきに、自分らしさも演出できる素材アイテム。 / ➀ 無垢面材のキッチン 「スイージー」 / ②調温や消臭機能で空間はいつも快適 「高千穂シラス壁」 / ③空気を洗う無垢床材「AirWashFlooring」" /></li>

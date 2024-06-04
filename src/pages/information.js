f6ae4c2f3837c628336f3eaa4fb33d32c07b736e
福田 翔太 <shouta@lct.jp>
2024-06-04 14:04:41 +0900

    更新

--- src/pages/information.js
+++ src/pages/information.js
@@ -25,17 +25,17 @@ const SecondPage = () => (
         <div className="article__contents">
           <div className="container">
             <section className="article__section article__section01">
-              <h2>選べる3つの暮らしパッケージ</h2>
+              <h2>暮らしで選ぶ、3つのおトクなパッケージ</h2>
               <p className="text-center h3 mb-5">これからの住空間にも馴染みやすいスタイルとしてシーンごとにデザイナーがパッケージ監修してご提案。</p>
-              <p className="text-center h3 mb-5"><a href="https://www.royal-house.co.jp/campaign/">シン・平屋で賢く暮らそうキャンペーン</a></p>
-              <p className="text-center h3 mb-5">期間：2023年10月1日(日)～2023年12月31日(日)</p>
+              <p className="text-center h3 mb-5"><a href="https://www.royal-house.co.jp/campaign/">安心の家づくりキャンペーン</a></p>
+              <p className="text-center h3 mb-5">期間：2024年4月1日（月）～2024年6月30日（日）</p>
               <ul className="info-bnr__list">
-                <li><h3>１ながら美容パッケージ</h3></li>
-                <li className="center"><img src={withPrefix("/images/information/livgrand_img01.jpg")} alt="１ながら美容パッケージ 毎日の習慣が健康と美容活動に！自宅が SPA空間に代わるアイテムと、浄活水で家中の水がミネラルウォーターの様になるアイテム。 / ➀微細な泡で体中キレイに 「ミラバス」 / ②シャワーヘッド型美顔器 「ミラブル ZERO」 / ③家中キレイな水を提供 「美fine ウォーター」" /></li>
-                <li><h3>2家事楽パッケージ</h3></li>
-                <li className="center"><img src={withPrefix("/images/information/livgrand_img02.jpg")} alt="2家事業パッケージ 時短・手間楽・キレイつづくアイテム!デザイン性が高く、高性能な設備を厳選。家を楽にストレスなく家事ができるアイテム。 / ➀自動洗浄で快適浴槽 「AX」 / ②家族で使えるキッチン「ES」 「ノクト」 / ③掃除も収納も楽ちん! 洗面化粧台 「EV」 / ④ トイレ空間がすっきり 「ペーシアハーモノ」" /></li>
-                <li><h3>3自然素材パッケージ</h3>{/* <p className="h3 mb-5">家事楽動線をデザインした家族でワイワイにぎやかな空間に</p> */}</li>
-                <li className="center"><img src={withPrefix("/images/information/livgrand_img03.jpg")} alt="3自然素材パッケージ 本物の素材感と快適さにこだわる無垢の木や塗り壁など素材にこだわる上質な空間へ空間をすてきに、自分らしさも演出できる素材アイテム。 / ➀ 無垢面材のキッチン 「スイージー」 / ②調温や消臭機能で空間はいつも快適 「高千穂シラス壁」 / ③空気を洗う無垢床材「AirWashFlooring」" /></li>
+                <li><h3>1 水からキレイパッケージ</h3></li>
+                <li className="center"><img src={withPrefix("/images/information/pack_img01.jpg")} alt="選べるお得な暮らしパック 水からキレイパッケージ　健康・美谷パッケージ。毎日、水と美容にこだわるサイエンスの人気3点セット　ミラブルゼロ / 美-fine-ウォーター / ミラバス" /></li>
+                <li><h3>2 こだわり収納パッケージ</h3></li>
+                <li className="center"><img src={withPrefix("/images/information/pack_img02.jpg")} alt="選べるお得な暮らしパック こだわり収納パッケージ　こだわりの収納パッケージ。空問活用収納で収納カアップ。有効スペースを立体的に活用。　ワークスペース / 本棚 / ウォークインクローゼット" /></li>
+                <li><h3>3 安心安全パッケージ</h3>{/* <p className="h3 mb-5">家事楽動線をデザインした家族でワイワイにぎやかな空間に</p> */}</li>
+                <li className="center"><img src={withPrefix("/images/information/pack_img03.jpg")} alt="選べるお得な暮らしパック 安心・安全パッケージ　宅配新時代に向けて、玄関周りのセキュリティー強化。ちょっとしたプラスの設備で安心安全は大幅アップ　宅配ボックス / 防犯カメラ付きインターホン（スマホ操作対応・録画機能付き）" /></li>
               </ul>
             </section>
           </div>
